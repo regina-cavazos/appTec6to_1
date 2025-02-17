7c1cbd19b24e32943e7af2fb9134c5f2da70d6d6
regina-cavazos <130869156+regina-cavazos@users.noreply.github.com>
2025-02-17 12:37:51 -0600

    pagina 2 en construccion

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,9 @@
+
+export default function page2() {
+  return (
+    <div className="flex items-center justify-center h-screen  ">
+    <h1 className="text-2xl font-bold">En Construcción</h1>
+    
+  </div>
+  );
+}
